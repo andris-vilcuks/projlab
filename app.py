0bd415338122c145a56b8edae6899fbe26823485
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-11-30 14:10:05 +0200

    1

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-# Datnes izvēlne 
-
 st.set_page_config(page_title="Projektēšanas laboratorija", page_icon=":basketball:")
 
+# Datnes izvēlne
 sagataves = st.checkbox(
     "Izmantot iepriekš sagatavotas tabulas", True, help="Use in-built example file to demo the app"
 )
 
+if sagataves == True:
+    datne = "NBA2022-23.csv"
+
 if sagataves == False:
     datne = st.file_uploader("Augšupielādēt CSV datni", type=".csv")
-
-
 
 if datne:
     df = pd.read_csv(datne)
