561e75cf5aa7d2efb9e8a9d4023ae2b58ef1f409
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-11-30 14:30:53 +0200

    1

--- app.py
+++ app.py
@@ -3,23 +3,35 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
+# Pārlūkprogrammas cilnes virsraksts un attēls
 st.set_page_config(page_title="Projektēšanas laboratorija", page_icon=":basketball:")
 
-# Datnes izvēlne
-sagataves = st.checkbox(
-    "Izmantot iepriekš sagatavotas tabulas", True, help="Use in-built example file to demo the app"
-)
+# Sānjoslas virsraksts (nepieciešams, lai jau sākumā attēlotu sānjoslu) 
+st.sidebar.header("Parametri")
 
+# Datnes izvēlne
+sagataves = st.checkbox("Izmantot iepriekš sagatavotas tabulas", True)
+
+# Ja ir ieķeksēts, tad izvēlas jau sagatavotu tabulu
 if sagataves == True:
     datne = "NBA2022-23.csv"
 
+# Ja izņem ķeksi, tad lietotājam nepieciešams augšupielādēt .csv datni patstāvīgi
 if sagataves == False:
     datne = st.file_uploader("Augšupielādēt CSV datni", type=".csv")
 
+# Brīdī, kad ir zināms, kura datne tiks lietota, notiek sekojošās darbības:
 if datne:
+    # Datnes nolasīšana
     df = pd.read_csv(datne)
+    komandas = raw.drop(["GP","W","L","WIN%","Min","FGM","FGA","FG%","3PM","3PA","3P%","FTM","FTA","FT%","OREB","DREB","TOV","BLK","BLKA","PF","PFD",], axis=1)
 
+    # Datu tabulas priekšskatījums
     st.header("Tabulas priekšskatījums")
     st.dataframe(df)
 
-    
+    # Mājnieku komandas izvēle
+    majnieki = sorted(playerstats.Tm.unique())
+    selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+
+    # Viesu komandas izvēle
