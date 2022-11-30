0a264d2245a687d9e670a6d59e365b03ce660377
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-11-30 11:15:16 +0200

    1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import scipy.stats
+from scipy.stats import norm
+import altair as alt
 
+st.set_page_config(
+    page_title="Projektēšanas laboratorija", page_icon=":basketball:", initial_sidebar_state="expanded"
+)
