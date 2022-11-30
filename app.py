506d184f0cff2b2eb8572f30f8f7e3b36277ccab
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-11-30 15:00:22 +0200

    1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Nepieciešamo moduļu pievienošana
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -15,7 +16,6 @@ sagataves = st.checkbox("Izmantot iepriekš sagatavotas tabulas", True)
 # Ja ir ieķeksēts, tad izvēlas jau sagatavotu tabulu
 if sagataves == True:
     datne = "NBA2022-23.csv"
-
 # Ja izņem ķeksi, tad lietotājam nepieciešams augšupielādēt .csv datni patstāvīgi
 if sagataves == False:
     datne = st.file_uploader("Augšupielādēt CSV datni", type=".csv")
@@ -24,19 +24,20 @@ if sagataves == False:
 if datne:
     # Datnes nolasīšana
     df = pd.read_csv(datne)
-
     # Nepieciešamo datu atlase (atmet liekos datus)
-    komandas = df.drop(["GP","W","L","WIN%","Min","FGM","FGA","FG%","3PM","3PA","3P%","FTM","FTA","FT%","OREB","DREB","TOV","BLK","BLKA","PF","PFD"], axis=1)
+    df_komandas = df.drop(["GP","W","L","WIN%","Min","FGM","FGA","FG%","3PM","3PA","3P%","FTM","FTA","FT%","OREB","DREB","TOV","BLK","BLKA","PF","PFD"], axis=1)
 
     # Datu tabulas priekšskatījums
     st.header("Tabulas priekšskatījums")
-    st.dataframe(komandas)
+    st.dataframe(df_komandas)
 
     # Sānjosla - Mājnieku komandas izvēle
-    majnieku_izvelne = sorted(komandas.Team.unique())
+    majnieku_izvelne = sorted(df_komandas.Team.unique())
     majnieki = st.sidebar.selectbox('Mājnieki', majnieku_izvelne)
 
     # Sānjosla - Viesu komandas izvēle
-    viesu_izvelne = sorted(komandas.Team.unique())
+    viesu_izvelne = sorted(df_komandas.Team.unique())
     viesi = st.sidebar.selectbox('Viesi', viesu_izvelne)
 
+    # Mājnieku tabulas priekšskatījums
+    # st.dataframe(df_majnieki)
