6e5ecfac02dbe47a0b0a05910f5ee857f07ffe29
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-11-30 14:03:41 +0200

    1

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
+# Datnes izvēlne 
+
 st.set_page_config(page_title="Projektēšanas laboratorija", page_icon=":basketball:")
 
+sagataves = st.checkbox(
+    "Izmantot iepriekš sagatavotas tabulas", False, help="Use in-built example file to demo the app"
+)
+
 datne = st.file_uploader("Augšupielādēt CSV datni", type=".csv")
+
+
 
 if datne:
     df = pd.read_csv(datne)
@@ -13,5 +21,4 @@ if datne:
     st.header("Tabulas priekšskatījums")
     st.dataframe(df)
 
-    st.sidebar.header('Lietotāja ievade')
-    gads = st.sidebar.selectbox('Gads', list(reversed(range(1950,2024))))
+    
