c6cca28e0534f63a4291dc2bd4c0e30a7dc18653
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-11-30 11:29:51 +0200

    1

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-st.set_page_config(
-    page_title="Projektēšanas laboratorija", page_icon=":basketball:", initial_sidebar_state="expanded"
-)
+st.set_page_config(page_title="Projektēšanas laboratorija", page_icon=":basketball:")
+
+datne = st.file_uploader("Augšupielādēt CSV datni", type=".csv")
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+
+    st.header("Tabulas priekšskatījums")
+    st.dataframe(df.head())
