65b946a63228c8115ebc8d143bb7fc40a4eb1bf9
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-01 12:29:29 +0200

    1

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ if datne:
 
     # Mājnieki
     # Sānjosla - komandas izvēle
-    majnieku_izvelne = sorted(df_komandas.Team.unique())
+    majnieku_izvelne = sorted(df_komandas['Team'].unique())
     majnieki = st.sidebar.selectbox('Mājnieki', majnieku_izvelne)
     st.text(majnieki)
     # Atlases tabulas priekšskatījums
-    df_majnieki = df_komandas.loc[majnieki]
+    df_majnieki = df_komandas.loc[(df_komandas['Team']=majnieki)]
     st.dataframe(df_majnieki)
 
     # Viesi
