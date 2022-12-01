cf00b46b2b4d337df0916275bbd7c9e49825b5cc
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-01 12:08:08 +0200

    1

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if datne:
     majnieku_izvelne = sorted(df_komandas.Team.unique())
     majnieki = st.sidebar.selectbox('Mājnieki', majnieku_izvelne)
     # Atlases tabulas priekšskatījums
-    df_majnieki = df_komandas.loc["Boston Celtics"]
+    df_majnieki = df_komandas.loc[["Boston Celtics"]]
     st.dataframe(df_majnieki)
 
     # Viesi
