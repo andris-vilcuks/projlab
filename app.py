05ddc5ee750bab0bc23cff70d3f3de36a007d7db
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-07 16:31:06 +0200

    1

--- app.py
+++ app.py
@@ -115,12 +115,12 @@ if datne:
 
     with rez0:
         # Mājnieku rezultāts
-        st.text("Mājnieki: " + majnieki)
-        st.text(a+b+c+d+e)
+        st.caption("Mājnieki: " + majnieki)
+        st.markdown(a+b+c+d+e)
     with rez1:
         # Viesu rezultāts
-        st.text("Viesi: " + viesi)
-        st.text(-a-b-c-d-e)
+        st.caption("Viesi: " + viesi)
+        st.markdown(-a-b-c-d-e)
 
     # Starpības
     with rez2:
