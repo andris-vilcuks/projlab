db20c9d02626046e231e7dce6c03af1053256fa7
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-07 15:50:46 +0200

    1

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if datne:
         st.dataframe(df_viesi)
 
     with col2:
-        st.line_chart(df_komandas, y="Team")
+        st.line_chart(df_komandas, x=['PTS','REB','AST','STL','+/-'], y="Team")
 
     # Mainīgo definēšana, noklusējuma vērtību uzstādīšana
     a = 0
