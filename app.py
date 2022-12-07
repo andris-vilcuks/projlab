93315d8f467358869eb6974e272360c9cffe7019
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-07 15:48:34 +0200

    1

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if datne:
         st.dataframe(df_viesi)
 
     with col2:
-        st.bar_chart(df_komandas, x="Team")
+        st.line_chart(df_komandas, x="Team")
 
     # Mainīgo definēšana, noklusējuma vērtību uzstādīšana
     a = 0
