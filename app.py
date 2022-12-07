b1869f509555d0ded876e384d7708a53ce847066
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-07 16:23:09 +0200

    1

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ datne = "NBA2022-23.csv"
 # Brīdī, kad ir zināms, kura datne tiks lietota, notiek sekojošās darbības:
 if datne:
 
-    # Satura sadalījums kolonnās
-    col1, col2 = st.columns(2)
-
     # Datnes nolasīšana
     df = pd.read_csv(datne)
     # Nepieciešamo datu atlase
@@ -33,91 +30,89 @@ if datne:
     viesi = st.sidebar.selectbox('Viesi', viesu_izvelne)
 
     # Datu tabulu priekšskatījums
-    with col1:
-        # Kopējo datu tabulas priekšskatījums
-        st.header("Tabulas priekšskatījums")
-        st.dataframe(df_komandas)
+    # Kopējo datu tabulas priekšskatījums
+    st.header("Tabulas priekšskatījums")
+    st.dataframe(df_komandas)
 
-        # Mājnieku atlases tabulas priekšskatījums
-        df_majnieki = df_komandas.loc[df_komandas["Team"] == majnieki]
-        st.header("Mājnieki:")
-        st.dataframe(df_majnieki)
-    
-        # Viesu atlases tabulas priekšskatījums
-        df_viesi = df_komandas.loc[df_komandas["Team"] == viesi]
-        st.header("Viesi:")
-        st.dataframe(df_viesi)
+    # Mājnieku atlases tabulas priekšskatījums
+    df_majnieki = df_komandas.loc[df_komandas["Team"] == majnieki]
+    st.header("Mājnieki:")
+    st.dataframe(df_majnieki)
+
+    # Viesu atlases tabulas priekšskatījums
+    df_viesi = df_komandas.loc[df_komandas["Team"] == viesi]
+    st.header("Viesi:")
+    st.dataframe(df_viesi)
 
     # Sānjosla - Kolonnas izvēle
     df_komandu_dati = df_komandas.drop(axis=1, columns='Team')
     kolonna = st.sidebar.selectbox('Kolonnas izvēle:', df_komandu_dati.columns)
 
-    with col2:
-        st.bar_chart(df_komandas, x="Team", y=kolonna)
+    st.bar_chart(df_komandas, x="Team", y=kolonna)
 
-        # Mainīgo definēšana, noklusējuma vērtību uzstādīšana
-        a = 0
-        b = 0
-        c = 0
-        d = 0
-        e = 0
+    # Mainīgo definēšana, noklusējuma vērtību uzstādīšana
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
 
-        # Vērtību atlase aprēķiniem
-        # Points(PTS) 5
-        m_pts = df_majnieki.iloc[0,1]
-        v_pts = df_viesi.iloc[0,1]
-        pts = m_pts - v_pts
-        if pts >= 0:
-            a = 5
-        else:
-            a = -5
+    # Vērtību atlase aprēķiniem
+    # Points(PTS) 5
+    m_pts = df_majnieki.iloc[0,1]
+    v_pts = df_viesi.iloc[0,1]
+    pts = m_pts - v_pts
+    if pts >= 0:
+        a = 5
+    else:
+        a = -5
 
-        # Rebounds(REB) 2
-        m_reb = df_majnieki.iloc[0,2]
-        v_reb = df_viesi.iloc[0,2]
-        reb = m_reb - v_reb
-        if reb >= 0:
-            b = 2
-        else:
-            b = -2
+    # Rebounds(REB) 2
+    m_reb = df_majnieki.iloc[0,2]
+    v_reb = df_viesi.iloc[0,2]
+    reb = m_reb - v_reb
+    if reb >= 0:
+        b = 2
+    else:
+        b = -2
 
-        # Assists(AST) 3
-        m_ast = df_majnieki.iloc[0,3]
-        v_ast = df_viesi.iloc[0,3]
-        ast = m_ast - v_ast
-        if ast >= 0:
-            c = 3
-        else:
-            c = -3
+    # Assists(AST) 3
+    m_ast = df_majnieki.iloc[0,3]
+    v_ast = df_viesi.iloc[0,3]
+    ast = m_ast - v_ast
+    if ast >= 0:
+        c = 3
+    else:
+        c = -3
 
-        # Steals(STL) 1
-        m_stl = df_majnieki.iloc[0,4]
-        v_stl = df_viesi.iloc[0,4]
-        stl = m_stl - v_stl
-        if stl >= 0:
-            d = 1
-        else:
-            d = -1
+    # Steals(STL) 1
+    m_stl = df_majnieki.iloc[0,4]
+    v_stl = df_viesi.iloc[0,4]
+    stl = m_stl - v_stl
+    if stl >= 0:
+        d = 1
+    else:
+        d = -1
 
-        # +/- 4
-        m_pm = df_majnieki.iloc[0,5]
-        v_pm = df_viesi.iloc[0,5]
-        pm = m_pm - v_pm
-        if pm >= 0:
-            e = 4
-        else:
-            e = -4  
+    # +/- 4
+    m_pm = df_majnieki.iloc[0,5]
+    v_pm = df_viesi.iloc[0,5]
+    pm = m_pm - v_pm
+    if pm >= 0:
+        e = 4
+    else:
+        e = -4  
 
-        # Attēlo galā iegūto rezultātu
-        # Mājnieku rezultāts
-        st.header(majnieki)
-        st.header(a+b+c+d+e)
-        # Viesu rezultāts
-        st.header(viesi)
-        st.header(-a-b-c-d-e)
-        # Starpības
-        st.metric("PTS", m_pts, pts)
-        st.metric("REB", m_reb, reb)
-        st.metric("AST", m_ast, ast)
-        st.metric("STL", m_stl, stl)
-        st.metric("+/-", m_pm, pm)
+    # Attēlo galā iegūto rezultātu
+    # Mājnieku rezultāts
+    st.header(majnieki)
+    st.header(a+b+c+d+e)
+    # Viesu rezultāts
+    st.header(viesi)
+    st.header(-a-b-c-d-e)
+    # Starpības
+    st.metric("PTS", m_pts, pts)
+    st.metric("REB", m_reb, reb)
+    st.metric("AST", m_ast, ast)
+    st.metric("STL", m_stl, stl)
+    st.metric("+/-", m_pm, pm)
