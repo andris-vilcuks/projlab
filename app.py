940a85f6aa0ddba4f211c6a34cdce52428f326e0
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-07 15:46:22 +0200

    1

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if datne:
         st.dataframe(df_viesi)
 
     with col2:
-        st.bar_chart(df_majnieki)
+        st.bar_chart(df_komandas)
 
     # Mainīgo definēšana, noklusējuma vērtību uzstādīšana
     a = 0
