c6cd7eba74f6bb3c92437fb4cb13a26f411cdb62
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-07 16:18:02 +0200

    1

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ if datne:
         st.dataframe(df_viesi)
 
     # Sānjosla - Kolonnas izvēle
-    st.header("Diagramma")
     df_komandu_dati = df_komandas.drop(axis=1, columns='Team')
     kolonna = st.sidebar.selectbox('Kolonnas izvēle:', df_komandu_dati.columns)
 
