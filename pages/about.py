627d4370b1ae7b20f8877ab704be201006e11bd0
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-07 05:32:21 +0200

    1

--- pages/about.py
+++ pages/about.py
@@ -1,6 +1,9 @@
+import streamlit as st
+
 st.set_page_config(page_title="Projektēšanas laboratorija", page_icon=":basketball:", layout="wide")
 
 st.title('Basketbola spēles rezultāta prognozēšana')
 
 st.markdown('Izplatīta metode, ko izmanto spēļu rezultātu prognozēšanai komandu sporta veidos, ir pazīstama kā “loģistisko regresiju”. Šis modelis izmanto vairākus mainīgos lielumus abām komandām un izveido formulu, ko var izmantot, lai aprēķinātu katras komandas uzvaras varbūtību. Izmantotie mainīgie parasti ietver spēlētāju vērtējumus, komandu reitingus un iepriekšējo sniegumu.')
 
+st.markdown(about.md)
