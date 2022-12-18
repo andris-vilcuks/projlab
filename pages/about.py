57cbed35cedade6d368549a26bc314786785cdb1
andris-vilcuks <54868662+andris-vilcuks@users.noreply.github.com>
2022-12-18 23:29:28 +0200

    1

--- pages/about.py
+++ pages/about.py
@@ -9,6 +9,13 @@ st.markdown('Izplatīta metode, ko izmanto spēļu rezultātu prognozēšanai ko
 
 st.header('Dati')
 st.markdown('Basketbola komandu dati tiek iegūti no vietnes https://www.nba.com/stats/teams/traditional , kurā ir pieejami gan pašreizējās, gan iepriekšējo sezonu komandu dati to apstrādei.')
+st.markdown('Pēc nepieciešamības, lietotājam ir iespēja arī ievietot savu .csv datni datu apstrādei. Lai būtu iespējama pilnvērtīga datu apstrāde, lietotājam ir nepieciešamas kolonnas ar nosaukumiem:')
+st.markdown('- TEAM')
+st.markdown('- PTS')
+st.markdown('- +/-')
+st.markdown('- AST')
+st.markdown('- REB')
+st.markdown('- STL')
 
 st.header('Formula')
 st.markdown('No iegūtajiem datiem, tiek atlasīti tikai svarīgākie un noteikts to nozīmīgums ar skaitlisku vērtību:')
@@ -16,7 +23,7 @@ st.markdown('- Punkti jeb *points* (PTS) - 5')
 st.markdown('- Plus/Mīnus (+/-) - 4')
 st.markdown('- Piespēles jeb *assists* (AST) - 3')
 st.markdown('- Atlēkušās bumbas jeb *rebounds* (REB) - 2')
-st.markdown('- Atņemtās bumbas jeb *steals* (PTS) - 1')
+st.markdown('- Atņemtās bumbas jeb *steals* (STL) - 1')
 
 st.header('Darbu veidoja:')
 st.markdown('Pauls Brikmanis 201RDB002')
